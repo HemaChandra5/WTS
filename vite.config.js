4ed06f70297d631a9c66474df3b002330304c104
Hema Chandra VK-0012 <hemachandra@sskatt.com>
2026-05-14 17:00:08 +0530

    render

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/FMS/' : '/',  // IMPORTANT: repo name
+  base: command === 'build' ? '/FMS/' : '/',
   build: {
-    outDir: 'docs',                          // IMPORTANT: docs
+    outDir: 'docs',
   },
   server: {
     port: 5173,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 10000,
+    allowedHosts: ['fms-1-2614.onrender.com'],
   },
 }));
